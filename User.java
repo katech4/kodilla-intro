101ac57111d47e6e6b5e62ace6b1ba88a1b2f6e7
Katarzyna Szymkowiak <katarzyna.chmielewska@hotmail.com>
2021-01-01 17:53:04 +0100

    User.java

--- User.java
+++ User.java
@@ -1,13 +1,17 @@
 public class User {
 
+    private String name;
+    private int age;
 
     public User(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public static void main(String[] args) {
 
         User katarzyna = new User("Katarzyna", 20);
-        User maciej = new User("Maciej", 33);
+        User maciej = new User("Maciej", 10);
         User michal = new User("Michal", 58);
 
         User[] users = {katarzyna, maciej, michal};
@@ -15,7 +19,19 @@ public class User {
 
         int result = 0;
         for (int i = 0; i < users.length; i++) {
-            /*result = result + users.length[i];*/
+            result = result + users[i].age;
+        }
+
+        double av = result / users.length;
+
+        System.out.println("Srednia wieku uzytkownikow: " + av);
+
+        for (int i=0; i< users.length; i++){
+             int age = users[i].age;
+
+             if (age < av){
+                 System.out.println("Imiona uzytkownika ponizej sredniej: " + users[i].name);
+             }
         }
     }
 }
